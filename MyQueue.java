24dc4b6669ccded97385ff49b8b21a3a1b3f2a34
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-10-24 22:11:14 +0800

    Add files via upload

--- MyQueue.java
+++ MyQueue.java
@@ -1,59 +1,65 @@
-package 栈;
-
-class Node{//表
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: GaoBo
+ * Date: 2021-06-04
+ * Time: 20:58
+ */
+class Node {
     private int val;
     private Node next;
-    public int getVal(){
+    public int getVal() {
         return val;
     }
-    public void setVal(){
-        this.val=val;
+    public void setVal(int val) {
+        this.val = val;
     }
-    public Node getNext(){
+    public Node getNext() {
         return next;
     }
-    public void setNext(Node node){
-        this.next=next;
+    public void setNext(Node next) {
+        this.next = next;
     }
-    public Node(int val){
-        this.val=val;
+    public Node(int val) {
+        this.val = val;
     }
 }
 public class MyQueue {
     private Node first;
     private Node last;
-    public boolean isEmpty(){
-        return this.first == null;
-    }
     //入队
-    public void offer(int val){
-        //尾插法 判断是否是不是第一次插入
-        Node node =new Node(val);
-        if(this.first == null){
+    public void offer(int val) {
+        //尾插法  需要判断是不是第一次插入
+        Node node = new Node(val);
+        if(this.first == null) {
             this.first = node;
             this.last = node;
-        }else{
-            this.last.setNext(node);
-            this.last =node;
+        }else {
+            this.last.setNext(node);//last.next = node;
+            this.last = node;
         }
     }
-
     //出队
-    public int poll(){
-        if(isEmpty()){
-            throw  new UnsupportedOperationException("队列空！！！");
+    public int poll() {
+        //1判断是否为空的
+        if(isEmpty()) {
+            throw new UnsupportedOperationException("队列为空！");
         }
-        int ret=this.first.getVal();
-        this.first=this.first.getNext();
+        //this.first = this.first.next;
+        int ret = this.first.getVal();
+        this.first = this.first.getNext();
         return ret;
     }
-    //得到队头元素不删除
-    public int peek(){
-        if(isEmpty()){
-            throw new UnsupportedOperationException("队列空！！");
+    //得到队头元素但是不删除
+    public int peek() {
+        //不要移动first
+        if(isEmpty()) {
+            throw new UnsupportedOperationException("队列为空！");
         }
         return this.first.getVal();
     }
-    //队列是否空
-
+    //队列是否为空
+    public boolean isEmpty() {
+        return this.first == null;
+    }
 }
--- TestDemo.java
+++ TestDemo.java
@@ -1,15 +1,108 @@
-package 树;
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
-import java.util.*;
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: GaoBo
+ * Date: 2021-06-04
+ * Time: 12:33
+ */
+
 public class TestDemo {
-//    /
-
 
     public static void main(String[] args) {
-        double a=4.0;
-        double v=(4/3.0)*3.14;
-        System.out.println(v);
+        MyQueue myQueue = new MyQueue();
+        myQueue.offer(1);
+        myQueue.offer(2);
+        myQueue.offer(3);
+
+        System.out.println(myQueue.peek());//1
+        System.out.println(myQueue.poll());//1
+        System.out.println(myQueue.poll());//2
+        System.out.println(myQueue.poll());//3
+
+        System.out.println(myQueue.isEmpty());//true
+
+        System.out.println(myQueue.poll());//
+
     }
 
+    public static void main4(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+
+
+        Deque<Integer> queue2 = new LinkedList<>();
+
+
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+
+        System.out.println(queue.peek());//1 得到队头元素
+        System.out.println(queue.poll());//1 出队
+        System.out.println(queue.poll());//2 出队
+
+        //LinkedList<Integer> linkedList = new LinkedList<>();
+
+    }
+
+    public static void main3(String[] args) {
+        MyStack<Integer> myStack = new MyStack<>();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        System.out.println(myStack.peek());//3
+        System.out.println(myStack.pop());//3
+        System.out.println(myStack.pop());//2
+        System.out.println(myStack.pop());//1
+        System.out.println(myStack.empty());//true
+        System.out.println("============================");
+        MyStack<String> myStack2 = new MyStack<>();
+        myStack2.push("hello");
+        myStack2.push("gaobo");
+        myStack2.push("cool");
+        System.out.println(myStack2.peek());
+        System.out.println(myStack2.pop());
+        System.out.println(myStack2.pop());
+        System.out.println(myStack2.pop());
+        System.out.println(myStack2.empty());
+
+
+
+    }
+    public static void main2(String[] args) {
+        MyStack myStack = new MyStack();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        System.out.println(myStack.peek());//3
+        System.out.println(myStack.pop());//3
+        System.out.println(myStack.pop());//2
+        System.out.println(myStack.pop());//1
+        System.out.println(myStack.empty());//true
+    }
+    public static void main1(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);//入栈
+        // 1 2 3
+        int ret = stack.pop();//删除栈顶的元素  出栈
+        System.out.println(ret);
+
+        int ret2 = stack.peek();//得到栈顶元素但是不删除
+        System.out.println(ret2);
+        stack.pop();//2
+        stack.pop();//1
+
+        boolean flg = stack.empty();
+        System.out.println(flg);
+    }
 }
