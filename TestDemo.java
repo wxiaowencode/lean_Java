c98da19322a61105bec327ff17287f86164ba0f4
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-10-27 12:36:05 +0800

    Add files via upload

--- TestDemo.java
+++ TestDemo.java
@@ -1,108 +1,154 @@
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-
 /**
  * Created with IntelliJ IDEA.
  * Description:
  * User: GaoBo
- * Date: 2021-06-04
- * Time: 12:33
+ * Date: 2021-04-27
+ * Time: 16:43
  */
-
 public class TestDemo {
 
+
     public static void main(String[] args) {
-        MyQueue myQueue = new MyQueue();
-        myQueue.offer(1);
-        myQueue.offer(2);
-        myQueue.offer(3);
-
-        System.out.println(myQueue.peek());//1
-        System.out.println(myQueue.poll());//1
-        System.out.println(myQueue.poll());//2
-        System.out.println(myQueue.poll());//3
-
-        System.out.println(myQueue.isEmpty());//true
-
-        System.out.println(myQueue.poll());//
-
+        TestLinkedList testLinkedList = new TestLinkedList();
+        testLinkedList.addLast(1);
+        testLinkedList.addLast(2);
+        testLinkedList.addLast(3);
+        testLinkedList.addLast(4);
+        testLinkedList.display();
     }
 
-    public static void main4(String[] args) {
 
-        LinkedList<Integer> linkedList = new LinkedList<>();
+    public static ListNode mergeTwoLists(ListNode headA, ListNode headB) {
+        ListNode newHead = new ListNode();//傀儡节点
+        ListNode tmp = newHead;
+        while (headA != null && headB != null) {
+            if(headA.val < headB.val) {
+                tmp.next = headA;
+                headA = headA.next;
+                tmp = tmp.next;
+            }else {
+                tmp.next = headB;
+                headB = headB.next;
+                tmp = tmp.next;
+            }
+        }
+        if(headA != null) {
+            tmp.next = headA;
+        }
+        if(headB != null) {
+            tmp.next = headB;
+        }
+        return newHead.next;
+    }
 
 
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
-        Deque<Integer> queue2 = new LinkedList<>();
+        //0、分别定义一个pl和ps来指定最长和最短的链表
+        //我假设刚开始的时候，pl指向的是最长的
+        ListNode pl = headA;
+        ListNode ps = headB;
+        //1、别求的两个链表的长度
+        int lenA = 0;
+        int lenB = 0;
+        while (pl != null) {
+            lenA++;
+            pl = pl.next;
+        }
+        while (ps != null) {
+            lenB++;
+            ps = ps.next;
+        }
+        //重点：因为为了求长度 这两个引用都是null了。
+        pl = headA;
+        ps = headB;
+        int len = lenA - lenB;
+        if(len < 0) {
+            pl = headB;
+            ps = headA;
+            len = lenB-lenA;
+        }
+        //2.pl指向的链表一定是最长的！！！！  len一定是那个长度差
 
+        //3、让pl所指向的链接走len步
 
+        while (len != 0) {
+            pl = pl.next;
+            len--;
+        }
+        //4、同时走 pl==ps==null
+        while (pl != ps) {
+            pl = pl.next;
+            ps = ps.next;
+        }
+        if(pl == null) {
+            return null;//没有交点
+        }
+        return pl;
+    }
 
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1);
-        queue.offer(2);
-        queue.offer(3);
-
-        System.out.println(queue.peek());//1 得到队头元素
-        System.out.println(queue.poll());//1 出队
-        System.out.println(queue.poll());//2 出队
-
-        //LinkedList<Integer> linkedList = new LinkedList<>();
-
+    public static void creteCut(ListNode headA, ListNode headB) {
+        headB.next.next = headA.next.next;
     }
 
     public static void main3(String[] args) {
-        MyStack<Integer> myStack = new MyStack<>();
-        myStack.push(1);
-        myStack.push(2);
-        myStack.push(3);
-        System.out.println(myStack.peek());//3
-        System.out.println(myStack.pop());//3
-        System.out.println(myStack.pop());//2
-        System.out.println(myStack.pop());//1
-        System.out.println(myStack.empty());//true
-        System.out.println("============================");
-        MyStack<String> myStack2 = new MyStack<>();
-        myStack2.push("hello");
-        myStack2.push("gaobo");
-        myStack2.push("cool");
-        System.out.println(myStack2.peek());
-        System.out.println(myStack2.pop());
-        System.out.println(myStack2.pop());
-        System.out.println(myStack2.pop());
-        System.out.println(myStack2.empty());
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addLast(1);
+        myLinkedList.addLast(3);
+        myLinkedList.addLast(36);
+        myLinkedList.addLast(10);
+        myLinkedList.display();
 
+        myLinkedList.createLoop();
 
-
-    }
-    public static void main2(String[] args) {
-        MyStack myStack = new MyStack();
-        myStack.push(1);
-        myStack.push(2);
-        myStack.push(3);
-        System.out.println(myStack.peek());//3
-        System.out.println(myStack.pop());//3
-        System.out.println(myStack.pop());//2
-        System.out.println(myStack.pop());//1
-        System.out.println(myStack.empty());//true
-    }
-    public static void main1(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);//入栈
-        // 1 2 3
-        int ret = stack.pop();//删除栈顶的元素  出栈
+        boolean ret = myLinkedList.hasCycle();
         System.out.println(ret);
 
-        int ret2 = stack.peek();//得到栈顶元素但是不删除
-        System.out.println(ret2);
-        stack.pop();//2
-        stack.pop();//1
+        ListNode node = myLinkedList.detectCycle();
+        System.out.println(node.val);
+    }
 
-        boolean flg = stack.empty();
-        System.out.println(flg);
+
+
+    public static void main2(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addLast(1);
+        myLinkedList.addLast(3);
+        myLinkedList.addLast(36);
+        myLinkedList.addLast(1);
+        myLinkedList.display();
+
+        MyLinkedList myLinkedList2 = new MyLinkedList();
+        myLinkedList2.addLast(1);
+        myLinkedList2.addLast(6);
+        myLinkedList2.addLast(8);
+        myLinkedList2.addLast(14);
+        myLinkedList2.addLast(34);
+        myLinkedList2.display();
+
+
+        creteCut(myLinkedList.head,myLinkedList2.head);
+        ListNode ret = getIntersectionNode(myLinkedList.head,myLinkedList2.head);
+        if(ret == null) {
+            System.out.println("没有相交!");
+        }else {
+            System.out.println(ret.val);
+        }
+
+
+       /* ListNode ret = mergeTwoLists(myLinkedList.head,myLinkedList2.head);
+        myLinkedList2.display2(ret);*/
+
+       /* System.out.println("===============");
+
+        ListNode ret = myLinkedList.findKthToTail(4);
+        System.out.println(ret.val);*/
+
+        /*ListNode ret = myLinkedList.reverseList();
+        myLinkedList.display2(ret);*/
+        /*myLinkedList.removeAllKey(1);
+        myLinkedList.display();//*/
+        /*boolean ret = myLinkedList.contains(11);
+        System.out.println(ret);*/
     }
 }
