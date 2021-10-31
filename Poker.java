8d82677a0ce90c69cbd841401e665b1ac71ab6c7
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-10-31 16:40:39 +0800

    Add files via upload

---
+++ Poker.java
@@ -0,0 +1,32 @@
+package 扑克牌;
+
+import java.util.Stack;
+
+
+
+
+
+
+//栈里的几种方法
+public class Poker {
+    public static void main(String[] args) {
+
+        int a[][]=new int[5][6];
+        System.out.println(a.length);
+        System.out.println(a[0].length);
+    }
+//    public static void main1(String[] args) {
+//        Stack<Integer> stack=new Stack<>();
+//        stack.push(1);
+//        stack.push(2);
+//        stack.push(3);//入栈
+//
+//        int ret=stack.pop();//删除栈顶的元素   出栈
+//        System.out.println(ret);
+//        int ret2=stack.peek();//得到栈顶元素但是不删除
+//        System.out.println(ret2);
+//
+//        boolean flg=stack.empty();
+//        System.out.println(flg);
+    }
+
