a86a9d6934472078170979bc5e74af79287b9d9c
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-11-15 19:56:21 +0800

    Add files via upload

---
+++ ThreadDemo92.java
@@ -0,0 +1,14 @@
+package 线程;
+
+public class ThreadDemo92 {
+
+    public static void main(String[] args) {
+        Thread t=new Thread(){
+            @Override
+            public void run() {
+
+            }
+        };
+    }
+}
+
