22536af7ace9ed9389788bcfc981f24e2c1aaf42
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-11-23 12:33:16 +0800

    Add files via upload

---
+++ FileDemo11.java
@@ -0,0 +1,17 @@
+package 文件和IO;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileDemo11 {
+    //尝试从文件中读取出中文，借助标准库中内置的处理字符集的方式
+    //Scanner 不光能从控制台读取标准输入，也可以从文件中读取数据。
+//    try(InputStream inputStream=new FileInputStream("./test.txt")){
+//
+//    }
+
+
+
+
+
+}
