9ef22675930a28d2464a8db4e60027115198d03f
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-11-27 21:51:26 +0800

    Add files via upload

--- MyStack.java
+++ MyStack.java
@@ -1,62 +1,49 @@
-import java.util.Arrays;
-
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: GaoBo
- * Date: 2021-06-04
- * Time: 19:53
- */
+package 栈;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.Stack;
 public class MyStack<T> {
+
     private T[] elem;//数组
-    private int top;//当前可以存放数据元素的下标-》栈顶指针
+    private int top;//栈顶指针,当前可以存放数据的下标
 
-    public MyStack() {
-        this.elem = (T[])new Object[10];
+    public MyStack(){
+        this.elem=(T[])new Object[10];
     }
-
-    /**
-     * 入栈操作
-     * @param item 入栈的元素
-     */
-    public void push(T item) {
-        //1、判断当前栈是否是满的
+    //入栈操作
+    public void push(T item){
+        //1.判断当前栈是否是满的
         if(isFull()){
-            this.elem = Arrays.copyOf(this.elem,2*this.elem.length);
+            this.elem=Arrays.copyOf(this.elem,2*this.elem.length);
+            //扩容
         }
-        //2、elem[top] = item  top++;
-        this.elem[this.top++] = item;
+        //2.elem[top] = item  top++;
+        this.elem[this.top++]=item;
     }
-
     public boolean isFull(){
         return this.elem.length == this.top;
     }
-
-    /**
-     * 出栈
-     * @return 出栈的元素
-     */
-    public T pop() {
-        if(empty()) {
-            throw new UnsupportedOperationException("栈为空！");
+    //出栈操作
+    public T pop(){
+        //1.判断栈是否为空
+        if(empty()){
+            throw new UnsupportedOperationException("栈为空！！");
         }
-        T ret = this.elem[this.top-1];
-        this.top--;//真正的改变了top的值
+        //2.抛出栈顶元素
+        T ret=this.elem[this.top-1];
+        this.top--;
         return ret;
-    }
-
-    /**
-     * 得到栈顶元素，但是不删除
-     * @return
-     */
-    public T peek() {
-        if(empty()) {
-            throw new UnsupportedOperationException("栈为空！");
-        }
-        //this.top--;//真正的改变了top的值
-        return this.elem[this.top-1];
     }
     public boolean empty(){
         return this.top == 0;
+    }
+    //得到栈顶元素，但是不删除
+    public T peek(){
+        if(empty()){
+            throw new UnsupportedOperationException("栈为空！! ");
+        }
+        return this.elem[this.top-1];
     }
 }
