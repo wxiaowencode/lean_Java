b7738798331c9ae1380971ac4c3c233c0fded39a
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-11-27 21:51:10 +0800

    Add files via upload

--- TestDemo.java
+++ TestDemo.java
@@ -1,154 +1,200 @@
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: GaoBo
- * Date: 2021-04-27
- * Time: 16:43
- */
-public class TestDemo {
+package 栈;
+import java.util.*;
+import java.util.Scanner;
+import java.math.*;
 
+class A{
 
-    public static void main(String[] args) {
-        TestLinkedList testLinkedList = new TestLinkedList();
-        testLinkedList.addLast(1);
-        testLinkedList.addLast(2);
-        testLinkedList.addLast(3);
-        testLinkedList.addLast(4);
-        testLinkedList.display();
-    }
+}
+class B extends A{
 
+}
+class C extends B{
 
-    public static ListNode mergeTwoLists(ListNode headA, ListNode headB) {
-        ListNode newHead = new ListNode();//傀儡节点
-        ListNode tmp = newHead;
-        while (headA != null && headB != null) {
-            if(headA.val < headB.val) {
-                tmp.next = headA;
-                headA = headA.next;
-                tmp = tmp.next;
-            }else {
-                tmp.next = headB;
-                headB = headB.next;
-                tmp = tmp.next;
+}
+class Solution{
+    public boolean isValid(String s){
+        if( s == null || s.length() == 0){
+            return true;
+        }
+        //定义一个栈，存放左括号
+        Stack<Character> stack =new Stack<>();
+        //开始遍历当前的字符串s
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            //判断当前的字符是哪个左括号，因为题上说，只有3种括号
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }else{
+                //进入else ，说明当前的i下标是一个右括号
+                //先判断当前的栈是否空
+                if(stack.empty()){
+                    System.out.println("右括号多！");
+                    return false;
+                }
+
+                char tmp=stack.peek();
+                if(tmp == '{' && ch == '}' ||tmp == '[' && ch == ']'||tmp == '(' && ch == ')' ){
+                    //出栈当前的左括号，说明匹配成功
+                    stack.pop();
+                }else{
+                    System.out.println("左右括号不匹配！！！");
+                    return false;
+                }
             }
         }
-        if(headA != null) {
-            tmp.next = headA;
+        if(!stack.empty()){
+            System.out.println("左括号多！！！");
+            return false;
         }
-        if(headB != null) {
-            tmp.next = headB;
-        }
-        return newHead.next;
+
+        return true;
+    }
+}
+class Cyinder{
+    private double r;
+    private double h;
+
+    public double area(double r,double h){
+        this.r=r;
+        this.h=h;
+        double area=3.14*(r*r);
+        return area;
+    }
+    public double volume(double r,double h){
+        this.r=r;
+        this.h=h;
+        double volume=3.14*(r*r)*h;
+        return volume;
+    }
+}
+class Point{
+    private double x;
+    private double y;
+
+    public Point(double x1, double y1) {
+    }
+    public double distance(Point p1,Point p2){
+        double distance=Math.sqrt(Math.abs((p1.x - p2.x)* (p1.x - p2.x)+(p1.y - p2.y)* (p1.y - p2.y)));
+        return distance;
+    }
+}
+
+
+public class TestDemo {
+    public static String change(String number,int from ,int to){
+        String str=new BigInteger(number,from).toString(to);
+        return str.toUpperCase();
     }
 
+    public static void main(String[] args) {
+      //  System.out.println(Short.SIZE);//位大小
+     //   System.out.println(Short.BYTES);//字节大小
 
-    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+      //  System.out.printf("%0",1234);
+     //   System.out.printf("%o",1234);
+        System.out.printf("%10s",Integer.parseInt("ABCDEF",16));
+//        Integer a=1234;
+//        System.out.println(change("1234",10,8));
+//       // System.out.printf("%0",1234);
+//     //   System.out.printf("%x",1234);
+//        System.out.println(' ');
+//       // System.out.printf("%oX",1234);
+//     //   System.out.printf("%0X",1234);
+//        System.out.println(' ');
 
-        //0、分别定义一个pl和ps来指定最长和最短的链表
-        //我假设刚开始的时候，pl指向的是最长的
-        ListNode pl = headA;
-        ListNode ps = headB;
-        //1、别求的两个链表的长度
-        int lenA = 0;
-        int lenB = 0;
-        while (pl != null) {
-            lenA++;
-            pl = pl.next;
-        }
-        while (ps != null) {
-            lenB++;
-            ps = ps.next;
-        }
-        //重点：因为为了求长度 这两个引用都是null了。
-        pl = headA;
-        ps = headB;
-        int len = lenA - lenB;
-        if(len < 0) {
-            pl = headB;
-            ps = headA;
-            len = lenB-lenA;
-        }
-        //2.pl指向的链表一定是最长的！！！！  len一定是那个长度差
 
-        //3、让pl所指向的链接走len步
-
-        while (len != 0) {
-            pl = pl.next;
-            len--;
-        }
-        //4、同时走 pl==ps==null
-        while (pl != ps) {
-            pl = pl.next;
-            ps = ps.next;
-        }
-        if(pl == null) {
-            return null;//没有交点
-        }
-        return pl;
     }
 
-    public static void creteCut(ListNode headA, ListNode headB) {
-        headB.next.next = headA.next.next;
+    public static void main12(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x1 = scanner.nextDouble();
+        double x2 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
+        double y2 = scanner.nextDouble();
+        Point p1 = new Point(x1, y1);
+        Point p2 = new Point(x2, y2);
+        System.out.println(p1.distance(p1, p2));
     }
+    public static void main11(String[] args) {
+        Cyinder cyinder=new Cyinder();
+        Scanner scanner=new Scanner(System.in);
+        double r=scanner.nextDouble();
+        double h=scanner.nextDouble();
+        System.out.println(cyinder.area(r,h));
+        System.out.println(cyinder.volume(r,h));
+    }
+    public static void main9(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        while(scanner.hasNext()){
+            String str=scanner.next();
+            StringBuilder sb=new StringBuilder();
+            for(int i=0;i<str.length();i++){
+                char ch=str.charAt(i);
+                if(!sb.toString().contains(ch+"")){
+                    sb.append(ch);
+                }
+            }
+            System.out.println(sb);
+        }
+    }
+    public static void main8(String[] args) {
+       Object o = new Object(){
 
+            @Override
+            public boolean equals(Object obj) {
+                return true;
+            }
+        };
+        System.out.println(o.equals("gaobo"));
+
+    }
+    public static void main7(String[] args) {
+         A a=new B();
+         A a1= new C();
+         B b=new C();
+    }
+    public static void main6(String[] args) {
+        MyQueue myQueue=new MyQueue();
+        myQueue.offer(1);
+        myQueue.offer(2);
+        myQueue.offer(3);
+        System.out.println(myQueue.peek());//1
+        System.out.println(myQueue.poll());// 1
+        System.out.println(myQueue.poll());//2
+    }
     public static void main3(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList();
-        myLinkedList.addLast(1);
-        myLinkedList.addLast(3);
-        myLinkedList.addLast(36);
-        myLinkedList.addLast(10);
-        myLinkedList.display();
+        LinkedList<Integer> linkedList=new LinkedList<>();
 
-        myLinkedList.createLoop();
-
-        boolean ret = myLinkedList.hasCycle();
-        System.out.println(ret);
-
-        ListNode node = myLinkedList.detectCycle();
-        System.out.println(node.val);
+        Deque<Integer> queue2 =new LinkedList<>();
+        Deque<Integer> queue =new LinkedList<>();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        System.out.println(queue.peek());//得到队头元素
+        System.out.println(queue.poll());//出队 1
+        System.out.println(queue.poll());//出队 2
     }
-
-
-
     public static void main2(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList();
-        myLinkedList.addLast(1);
-        myLinkedList.addLast(3);
-        myLinkedList.addLast(36);
-        myLinkedList.addLast(1);
-        myLinkedList.display();
-
-        MyLinkedList myLinkedList2 = new MyLinkedList();
-        myLinkedList2.addLast(1);
-        myLinkedList2.addLast(6);
-        myLinkedList2.addLast(8);
-        myLinkedList2.addLast(14);
-        myLinkedList2.addLast(34);
-        myLinkedList2.display();
-
-
-        creteCut(myLinkedList.head,myLinkedList2.head);
-        ListNode ret = getIntersectionNode(myLinkedList.head,myLinkedList2.head);
-        if(ret == null) {
-            System.out.println("没有相交!");
-        }else {
-            System.out.println(ret.val);
-        }
-
-
-       /* ListNode ret = mergeTwoLists(myLinkedList.head,myLinkedList2.head);
-        myLinkedList2.display2(ret);*/
-
-       /* System.out.println("===============");
-
-        ListNode ret = myLinkedList.findKthToTail(4);
-        System.out.println(ret.val);*/
-
-        /*ListNode ret = myLinkedList.reverseList();
-        myLinkedList.display2(ret);*/
-        /*myLinkedList.removeAllKey(1);
-        myLinkedList.display();//*/
-        /*boolean ret = myLinkedList.contains(11);
-        System.out.println(ret);*/
+        MyStack<String> myStack2=new MyStack();
+        myStack2.push("hello");
+        myStack2.push("gaobo");
+        myStack2.push("cool");
+        System.out.println(myStack2.peek());//cool
+        System.out.println(myStack2.pop());//cool
+        System.out.println(myStack2.pop());//gaobo
+        System.out.println(myStack2.pop());//hello
+        System.out.println(myStack2.empty());//是否为空
+    }
+    public static void main1(String[] args) {
+        MyStack myStack=new MyStack();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        System.out.println(myStack.peek());//3
+        System.out.println(myStack.pop());//3
+        System.out.println(myStack.pop());//3
+        System.out.println(myStack.pop());//1
+        System.out.println(myStack.empty());//true
     }
 }
