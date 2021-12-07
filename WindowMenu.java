d1bd4dd004ae8ace399aff53131155e3ddd0c025
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-12-07 16:20:21 +0800

    Add files via upload

---
+++ WindowMenu.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+public class WindowMenu extends JFrame {
+    JMenuBar menubar;
+    JMenu menu,subMenu;
+    JMenuItem item1,item2;
+    public WindowMenu(){}
+    public WindowMenu(String s,int x,int y,int w,int h){
+          init(s);
+          setLocation(x,y);
+          setSize(w,h);
+          setVisible(true);
+          setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+    void init(String s){
+        setTitle(s);
+        menubar=new JMenuBar();
+        menu=new JMenu("菜单");
+        subMenu=new JMenu("软件项目");
+        item1=new JMenuItem("Java话题");
+        item2=new JMenuItem("动画话题");
+        item1.setAccelerator(KeyStroke.getKeyStroke('A'));
+        item2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+
+        menu.add(item1);
+        menu.addSeparator();
+        menu.add(item2);
+        menu.add(subMenu);
+        subMenu.add(new JMenuItem("汽车销售系统"));
+        subMenu.add(new JMenuItem("农场信息系统"));
+        menubar.add(menu);
+        setJMenuBar(menubar);
+
+    }
+}
