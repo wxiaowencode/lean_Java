3b794caf4b298f3efc78b1d1bf4000281dece92c
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-12-14 12:13:03 +0800

    Add files via upload

---
+++ SubAccountTable.java
@@ -0,0 +1,36 @@
+
+class CountNumNode{
+    private int countNumber;
+    private String name;
+    private int balNumber;
+
+
+    public int getCountNumber() {
+        return countNumber;
+    }
+
+    public void setCountNumber(int countNumber) {
+        this.countNumber = countNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalNumber() {
+        return balNumber;
+    }
+
+    public void setBalNumber(int balNumber) {
+        this.balNumber = balNumber;
+    }
+}
+
+
+
+public class SubAccountTable {
+}
