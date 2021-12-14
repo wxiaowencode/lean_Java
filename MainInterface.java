fbf2613877c9a0f216eb50d30e94d302f7945226
wxiaowencode <77837213+wxiaowencode@users.noreply.github.com>
2021-12-14 12:13:53 +0800

    Add files via upload

---
+++ MainInterface.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class MainInterface {
+
+
+
+    JMenuBar jb=new JMenuBar();
+    JMenu nMenu=new JMenu("系统");
+    JMenu mMenu=new JMenu("账户");
+
+
+
+
+
+
+
+
+
+    public void init(){
+
+    }
+
+
+    public static void main(String[] args) {
+        new MainInterface().init();
+    }
+}
